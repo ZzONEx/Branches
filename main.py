143da20beb4f373330b544a455b9fb1f3ab0b8ce
ZzONEx <mnakosergej@gmail.com>
2022-12-03 00:19:56 +0300

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+def my_superfunction():
+    print('what an awesome print!')
+def main():
+    print('My first git program')
+    print('And i change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+if __name__ == '__main__':
+    main()
